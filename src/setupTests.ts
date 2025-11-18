ef4ae2be758e60bf637bdb654b496ec50e3f5948
a.kashina <a.kashina@websecret.by>
2025-11-18 19:50:45 +0100

    feat: add dexie and fake-indexeddb for improved testing support

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,27 @@
 import '@testing-library/jest-dom/vitest';
 import { cleanup, configure } from '@testing-library/react';
+import { IDBKeyRange, indexedDB } from 'fake-indexeddb';
 import { afterEach } from 'vitest';
+
+if (!globalThis.indexedDB) {
+  globalThis.indexedDB = indexedDB as unknown as IDBFactory;
+}
+
+if (!globalThis.IDBKeyRange) {
+  globalThis.IDBKeyRange = IDBKeyRange as unknown as typeof globalThis.IDBKeyRange;
+}
+
+if (!Element.prototype.hasPointerCapture) {
+  Element.prototype.hasPointerCapture = () => false;
+}
+
+if (!Element.prototype.setPointerCapture) {
+  Element.prototype.setPointerCapture = () => {};
+}
+
+if (!Element.prototype.releasePointerCapture) {
+  Element.prototype.releasePointerCapture = () => {};
+}
 
 configure({ testIdAttribute: 'data-test-id' });
 
