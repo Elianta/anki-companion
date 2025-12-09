6c3b8f179061df127cb497a5e46ec6376d883b4c
a.kashina <a.kashina@websecret.by>
2025-12-09 14:48:03 +0100

    build: setup pwa plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,20 @@
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      includeAssets: ['site.webmanifest'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
